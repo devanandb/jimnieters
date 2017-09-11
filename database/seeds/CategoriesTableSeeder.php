b3fbb9705a1112c50ced09f9f7e0ae1b909c712c
Devanand <devanand.b@hp.com>
2017-09-11 05:56:32 -0700

    More updates

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -16,7 +16,7 @@ class CategoriesTableSeeder extends Seeder
                 'title' => 'Leadership',
                 'slug' => 'leadership',
                 'description' => 'Great UX leaders inspire world-class design. They inspire the best talent to do the best work of their careers. They speak the language of business, so they can inspire leaders and stakeholders, and show how great design both delights and monetizes.',
-                'image' => 'http://cms.designjedi.co/sites/default/files/Jim-Leader-Yodel-Version-2.png',
+                'image' => '/images/Jim-Leader-Yodel-Version-2.png',
                 'published' => true,
                 'created_at' => date("Y-m-d H:i:s"),
                 'updated_at' => date("Y-m-d H:i:s")
@@ -25,7 +25,7 @@ class CategoriesTableSeeder extends Seeder
                 'title' => 'Design',
                 'slug' => 'design',
                 'description' => 'If your product or service doesn’t delight, inspire, and engage, it’s a missed opportunity. Great design disrupts markets, from consumer electronics and gadgets, to apparel, to apps. They solve unmet and unstated user needs, and engage their users emotionally. Users love them!',
-                'image' => 'http://cms.designjedi.co/sites/default/files/jdesign.jpeg',
+                'image' => '/images/jdesign.jpeg',
                 'published' => true,
                 'created_at' => date("Y-m-d H:i:s"),
                 'updated_at' => date("Y-m-d H:i:s")
@@ -34,7 +34,7 @@ class CategoriesTableSeeder extends Seeder
                 'title' => 'Online',
                 'slug' => 'online',
                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti hic unde, veritatis dolorum labore itaque quod fugit reprehenderit deleniti. Officiis!',
-                'image' => 'http://cms.designjedi.co/sites/default/files/jdesign.jpeg',
+                'image' => '/images/jdesign.jpeg',
                 'published' => true,
                 'created_at' => date("Y-m-d H:i:s"),
                 'updated_at' => date("Y-m-d H:i:s")
